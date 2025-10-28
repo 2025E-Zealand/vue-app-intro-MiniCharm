83b0d059ac20b7f6f72a08cc98aaa49bb5d87508
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-28 12:42:48 +0100

    Store tilføjelser: Liste med eleversfornavn, efternavn og aldder.
    Efternavn kan vises eller kjulves
    
    indtastning af mobilnummer

--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            intro: 'Welcome to my Vue template',
-        }
-    },
-    methods: {
-        myMethod(){
+const app = Vue.createApp({ 
+    data() 
+    { return { 
+        intro: 'Welcome to my Vue template', 
+        Navn: 'Else', 
+        Efternavn: 'Munk', 
+        VisEfternavn:false, 
+        nr:'',
+        Studerende: [
+            {SFornavn:'Else',sEfternavn:'Munk',Alder: 20,VissEfternavn: false,mobilNr:'75444'}, 
+            {SFornavn:'Mille',sEfternavn:'Skov',Alder: 18,VissEfternavn: false,mobilNr:'8855'}, 
+            {SFornavn:'Magnus',sEfternavn:'Lund',Alder: 27,VissEfternavn: false,mobilNr:'86525'}, 
+            {SFornavn:'oliver',sEfternavn:'Skovbjerg',Alder:32,VissEfternavn: false,mobilNr:'83252'} 
 
-        },
-    },
-    computed: {
-        myComputed() {
-            return ''
-        },
-        
-    }
+        ] } 
+    }, 
+    methods: 
+    {  
+        vis(){ 
+            if(this.VisEfternavn==true) 
+            { 
+                this.VisEfternavn=false 
+            } 
+            else 
+            { 
+                this.VisEfternavn=true 
+            } 
+            },
+        ViSkjulStuderendeEfternavn(index){ 
+                if(this.Studerende[index].VissEfternavn==true) 
+                { 
+                    this.Studerende[index].VissEfternavn=false 
+                } 
+                else 
+                { 
+                    this.Studerende[index].VissEfternavn=true 
+                } 
+        } }, 
+    computed:
+    { 
+        myComputed() 
+        { 
+            return '' 
+        }, 
+        combinedName() { 
+            return this.Navn +' '+ this.Efternavn + ' ' 
+        }, 
+        StudentName()
+        { 
+            return this.Studerende.sEfternavn 
+        }, 
+        StudentEdternavn()
+        { 
+            return this.Studerende.sEfternavn 
+        } 
+    } 
 })
