7a041df205834272a2038d0ecab7a8207d041b20
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-29 16:12:20 +0100

    Ny studerende kan tilføjes til liste og array af jason objekter

--- js/main.js
+++ js/main.js
@@ -6,13 +6,22 @@ const app = Vue.createApp({
         Efternavn: 'Munk', 
         VisEfternavn:false, 
         nr:'',
+        //Data relateret til studerende som jason objekter(Benytter VisEfternavn fra tidligere data sektion)
+        sNavn:'',
+        sEfterNavn:'',
+        sAlder:'',
+        sMobil:'',
         Studerende: [
             {SFornavn:'Else',sEfternavn:'Munk',Alder: 20,VissEfternavn: false,mobilNr:'75444'}, 
             {SFornavn:'Mille',sEfternavn:'Skov',Alder: 18,VissEfternavn: false,mobilNr:'8855'}, 
             {SFornavn:'Magnus',sEfternavn:'Lund',Alder: 27,VissEfternavn: false,mobilNr:'86525'}, 
-            {SFornavn:'oliver',sEfternavn:'Skovbjerg',Alder:32,VissEfternavn: false,mobilNr:'83252'} 
+            {SFornavn:'Oliver',sEfternavn:'Skovbjerg',Alder:32,VissEfternavn: false,mobilNr:'83252'} 
 
-        ] } 
+        ],
+        //Data relateret til Studerende som array
+        StuderendeArray: [{Afornavn:'Else Munk'},{Afornavn:'Mille Skov'},{Afornavn:'Magnus Lund'},{Afornavn:'Oliver Skovbjerg'} ],
+        ANavn:'', 
+    } 
     }, 
     methods: 
     {  
@@ -27,15 +36,33 @@ const app = Vue.createApp({
             } 
             },
         ViSkjulStuderendeEfternavn(index){ 
-                if(this.Studerende[index].VissEfternavn==true) 
-                { 
-                    this.Studerende[index].VissEfternavn=false 
-                } 
-                else 
-                { 
-                    this.Studerende[index].VissEfternavn=true 
-                } 
-        } }, 
+            if(this.Studerende[index].VissEfternavn==true) 
+            { 
+                this.Studerende[index].VissEfternavn=false 
+            } 
+            else 
+            { 
+                this.Studerende[index].VissEfternavn=true 
+            } 
+        }, 
+        AddStuderendeArray(){
+            this.StuderendeArray.push({Afornavn:this.ANavn})
+            this.ANavn=''
+        },
+        sAddStuderende(){
+            this.Studerende.push({
+                                    SFornavn:this.sNavn,
+                                    sEfternavn:this.sEfterNavn,
+                                    Alder:this.sAlder,
+                                    VissEfternavn:this.VisEfternavn,
+                                    mobilNr:this.sMobil
+                                })
+            this.sNavn=''
+            this.sEfterNavn=''
+            this.sAlder=''
+            this.sMobil=''
+        }
+    }, 
     computed:
     { 
         myComputed() 
